a5a279fc6bc165d108862cefa3b5ed8ed183ed91
robert <rhsu123@gmail.com>
2015-07-04 20:10:48 -0400

    backup

--- src/junit/tutorial/JUnitTutorial.java
+++ src/junit/tutorial/JUnitTutorial.java
@@ -1,21 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package junit.tutorial;
 
-/**
- *
- * @author robert
- */
-public class JUnitTutorial {
+import junit.framework.*;
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args) {
-		// TODO code application logic here
+public class JUnitTutorial
+{
+	public String getName()
+	{
+		return "Hello World";
 	}
-	
 }
---
+++ test/junit/tutorial/TestClassOne.java
@@ -0,0 +1,21 @@
+package junit.tutorial;
+
+import junit.framework.TestCase;
+
+public class TestClassOne extends TestCase
+{
+	public TestClassOne(String method)
+	{
+		super(method);
+	}
+	
+	public void testAddition()
+	{
+		assertEquals(4, 2+2);
+	}
+	
+	public void testSubtraction()
+	{
+		assertEquals(0, 2-2);
+	}
+}
